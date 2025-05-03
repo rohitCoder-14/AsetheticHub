9591cf4410345fc32dcc8ffe323564f7c3c61aa4
rohitCoder-14 <rawatrohitsingh72@gmail.com>
2025-05-03 19:09:49 +0530

    Done

--- server/server.js
+++ server/server.js
@@ -18,7 +18,10 @@ const port = process.env.PORT || 4000;
 await connectDB();
 await connectCloudinary();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://asethetic-hub.vercel.app",
+];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebHooks);
 
