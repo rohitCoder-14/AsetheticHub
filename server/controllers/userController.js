9e949ab6205149263a94cddf558f85c5c485e3b5
rohitCoder-14 <rawatrohitsingh72@gmail.com>
2025-05-14 19:38:57 +0530

    User

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -87,7 +87,8 @@ export const login = async (req, res) => {
 //Check Auth : /api/user/is-auth
 export const isAuth = async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select("-password");
+    const userId = req.user._id;
+    const user = await User.findById(userId).select("-password");
     return res.json({ success: true, user });
   } catch (error) {
     console.log(error.message);
